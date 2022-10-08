9dd85485092258955f495944372430d9a1d07d91
cristinaretamal <cristinaretamal@gmail.com>
2022-10-08 15:52:58 +0200

    first commit

---
+++ Cristina Retamal/Evaluation1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Développer une application console permettant de jouer au pendu ou au démineur.
+
+Faire un "Fork" de ce dépôt
+Créer un dossier avec votre "Nom Prénom" à la racine
+Développer l'application dans ce dossier
+Sauver l'avancement avec des "Commits"
+Faire une "Pull request" avant le 14/10/2022 à minuit*/
+

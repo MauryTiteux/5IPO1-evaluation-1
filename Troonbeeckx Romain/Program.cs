ddcdfe70dbf38b6a65b554725e85505fd6b46b00
romain.trnbx <romain.troonbeeckx@gmail.com>
2022-10-08 13:05:17 +0200

    variables

--- Troonbeeckx Romain/Program.cs
+++ Troonbeeckx Romain/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.Write("hello");
+﻿Console.WriteLine("Entrez le mot à trouver");
+string mot = Console.ReadLine();
+int tailleMot = mot.Length;
+
+Console.WriteLine("le mot devra être trouvé en combien d'éssais?");
+string essais = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("le mot était "+ mot +" et il faisaitt "+ tailleMot+ " lettres");

440b6f8c71ae5348a439ec1bb7d89e40d71bccbb
Lukraste <49816981+Lukraste@users.noreply.github.com>
2022-10-08 11:17:18 +0200

    Création du projet

---
+++ Lebègue Sébastien/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class App
+{
+    public static void Main(string[] args)
+    {
+        Console.Title = "Jeu du pendu";
+        Console.ReadKey(true);
+
+        string hiddenWord = "Chouffe";
+        bool check = false;
+        string response = "";
+
+    }
+}

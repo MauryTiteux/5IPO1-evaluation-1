58029d83252b384b33866e3df48680345fe444be
OzgurGND <ozodaz@yahoo.fr>
2022-10-08 12:51:34 +0200

    Creation d'une List de mots

--- Gundogan Ozgur/Program.cs
+++ Gundogan Ozgur/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Bonhomme pendu");
+﻿using System.Transactions;
+
+Console.WriteLine("Bonhomme pendu");
 
 string mot1 = "maison";
 string mot2 = "camion";
@@ -7,11 +9,18 @@ string mot4 = "papier";
 string mot5 = "carton";
 string mot6 = "bouteille";
 
+List<string> listMots = new List<string>() { "maison", "camion", "pomme", "papier", "carton", "bouteille"};
+
 Random aleatoire = new Random();
 int numero;
-for(int i = 0; i<50; i++)
+for(int i = 0; i<10; i++)
 {
     numero = aleatoire.Next(1, 7); //numero allant de 1 -> 6
     Console.WriteLine(numero);
+}
+
+foreach(string mot in listMots)
+{
+    Console.WriteLine(mot);
 }
 

a6101d54a220365ee8b2fb814602e9c4e14524a8
OzgurGND <ozodaz@yahoo.fr>
2022-10-09 18:03:43 +0200

    Ajout Dessin; Creation Structure

--- Gundogan Ozgur/Program.cs
+++ Gundogan Ozgur/Program.cs
@@ -1,26 +1,61 @@
-﻿using System.Transactions;
+﻿// Cours : POO 
+//
+// Jeu : Bonhomme pendu
+//
+// Auteur : Ozgur GUNDOGAN
+// Prof : Jeremy KAIRIS
+//
 
-Console.WriteLine("Bonhomme pendu");
 
-string mot1 = "maison";
-string mot2 = "camion";
-string mot3 = "pomme";
-string mot4 = "papier";
-string mot5 = "carton";
-string mot6 = "bouteille";
+JeuStruct pendu = new JeuStruct("Bonhomme Pendu",0);
 
-List<string> listMots = new List<string>() { "maison", "camion", "pomme", "papier", "carton", "bouteille"};
+
+Console.Write("Jeu : "+pendu.GetTitre() + "        Niveau : " + pendu.GetNiveau());
+Console.WriteLine("        Vies : " + pendu.GetLives());
+
 
 Random aleatoire = new Random();
 int numero;
-for(int i = 0; i<10; i++)
+numero = aleatoire.Next(1, 7); //numero allant de 1 -> 6
+
+
+foreach (string dessinPart in pendu.GetDessinGlobal())
 {
-    numero = aleatoire.Next(1, 7); //numero allant de 1 -> 6
-    Console.WriteLine(numero);
+    Console.WriteLine(dessinPart); 
 }
 
-foreach(string mot in listMots)
+struct JeuStruct
 {
-    Console.WriteLine(mot);
+    private string titre;
+    private int niveau;
+    private int lives=6;
+    List<int> numeroFait = new List<int>();
+    List<string> listMots = new List<string>() { "maison", "camion", "pomme", "papier", "carton", "bouteille" };
+    string[] dessinGlobal = new string[] { " ____  ", " |  |  ", " |     ", " |     ", " |     ", " |     ", "------ " };
+    string dessinBonhomme = new string("O|/\\/\\");
+    List<int[]> coordonneesBonhomme = new List<int[]>() { new int[]{ 4, 2 }, new int[]{ 4, 3 } , new int[]{ 3, 3 },
+                                       new int[]{ 5, 3 }, new int[]{ 3, 4 }, new int[]{ 5, 4 } };
+    public JeuStruct(string titre, int niveau)
+    {
+        this.titre = titre;
+        this.niveau = niveau;
+    }
+    public string GetTitre()
+    {
+        return titre;
+    }
+    public int GetNiveau()
+    {
+        return niveau;
+    }
+    public int GetLives()
+    {
+        return lives;
+    }
+    public string[] GetDessinGlobal()
+    {
+        return dessinGlobal;
+    }
 }
+
 

e8591c6b4345ec11ab447e110212e1757440009c
WonderMaker87 <jeremy.don@gmx.com>
2022-10-14 18:17:56 +0200

    Système de vie

--- Jérémy Don/Program.cs
+++ Jérémy Don/Program.cs
@@ -1,4 +1,51 @@
 ﻿        int vie = 7;
-        string mot = "trouver";
-        string[] hide = new string[mot.Length];
-        string[] motArray = new string[mot.Length];
+        string word = "trouver";
+        string[] hide = new string[word.Length];
+        char[] wordArray = new char[word.Length];
+        string hiddenWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            wordArray[i]=word[i];
+        }
+
+        foreach (var item in wordArray)
+        {
+            hiddenWord = hiddenWord + "_";
+        }
+        void writeWord(){
+            Console.WriteLine(hiddenWord);
+        }
+
+
+        while (true)
+        {
+            Console.WriteLine("Entrer une Lettre : ");
+            char lettre = char.Parse(Console.ReadLine());
+            bool balise = false;
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                if(wordArray[i].Equals(lettre)){
+                    balise=true;
+                    hiddenWord = "";
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if(i==j){
+                            hiddenWord= hiddenWord + lettre;
+                        }else{
+                            hiddenWord = hiddenWord + "_";
+                        }
+                    }
+                }
+            }
+            if (balise==false)
+            {
+                vie--;
+            }
+            Console.WriteLine("Vie(s) restante(s) " + vie);
+            writeWord();
+            if(vie<=0){
+                Console.WriteLine("Game Over!");
+                break;
+                }
+        }

2edee9e5fc344248abc36db69f4260d7b51c5965
PapyChartreuse <m.louveaux@gmail.com>
2022-10-14 12:27:51 +0200

    supression structure et adaptation fonction pour array

--- Louveaux Mathieu/Louveaux Mathieu/Program.cs
+++ Louveaux Mathieu/Louveaux Mathieu/Program.cs
@@ -1,58 +1,119 @@
-﻿string str = "mathieu";
-int count = 3;
+﻿List<string> listMotPendu = new List<string> {"programation","pendu","objet","craie","australopitheque","bachibouzouk"};
+var random = new Random();
+int index = random.Next(listMotPendu.Count);                    //choisis un mot au hazard dans la liste
+//Console.WriteLine(listMotPendu[index]);
+string mot = listMotPendu[index];
+List<string> lettreUsed = new List<string> { };
+                             
+char[] test3 = motPendu(mot);                                   //transforme le mot en tableau de lettre
+string[] motArr = Array.ConvertAll(test3, x => x.ToString());   //convertis les lettre en string
+                   
+string[] motMasque = hiddenWord(motArr);                        // crée un tableau de meme longeur que le tableau des lettres du mot et le remplis de point
 
-static void attempt(string str = "mathieu", int count = 3)
+
+static void attempt(string[] arrMot, string[] arrMotCache, List<string> usedLettre, int count = 3, int occur = 0, int index = 0)
 {
     do
     {
-        char test = Console.ReadLine()[0];
-        //Char.TryParse(Console.ReadLine(), out test);
-        bool flag = false;
-        foreach (char c in str)
+        for (int i = 0; i < arrMotCache.Length; i++)                // affiche le nombre de lettre du mot a trouvé
         {
-            if (c == test)
+
+            Console.Write(arrMotCache.GetValue(i));
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Lettre deja utilisée");
+        usedLettre.ForEach(Console.Write);                          // affiche toute les lettres entrée par l'utilisateur
+        Console.WriteLine("");
+        Console.WriteLine("_______________");
+        char lettreChar = Console.ReadKey().KeyChar;
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        string lettreString = lettreChar.ToString();                // convertis le Char entré par l'utilisateur en string
+        string indexString = index.ToString();                      // convertis la variable index en string
+        usedLettre.Add(lettreString);                               // Ajouter la lettre entrée par l'utilsateur dans le tableau usedLettre
+
+        bool bonneLettre = false;                                   // flag va me permettre de savoir si j'ai trouver une lettre correspondante ou pas
+ 
+        for (int key = 0; key < arrMot.Length; key++)               // je boucle sur le tableau de mon mot a trouver
+        {
+            if (arrMot[key] == lettreString)                        // si la valeur de mon tableau mot a trouvera l'index "key" = la lettre entré par l'utilisateur
             {
-                Console.WriteLine("Bonne lettre");
-                flag = true;
+                if (arrMotCache[key] == lettreString)               // si la valeur de mon tableau mot cacher a l'index "key" = la lettre entré par l'utilisateur
+                {
+                    if (!bonneLettre)                               // si cette lettre a deja ete rentré dans le tableau mot caché je signale a l'utilisateur qu'il l'a deja entré
+                    {
+                        Console.WriteLine("Vous avez deja entre cette lettre");
+                    }
+                }
+                else                                                // si cette lettre n'a pas encore ete rentré dans le tableau mot caché
+                {
+                    if (!bonneLettre)                              // si cette lettre se trouve plusieurs fois dans le mot a trouver on ne signalera bonne lettre qu'une seul fois
+                    {
+                        Console.WriteLine("Bonne lettre");
+                    }
+                    arrMotCache[key] = arrMot[key];                 // on place la bonne lettre trouver au bon endroit dans le tableau mot caché
+                    occur++;                                        // incrémentation d'une de mes deux condition de sortie
+                    Console.WriteLine("");
+                }
+                bonneLettre = true;                                 // une bonne lettre a ete trouver => modification du booleen
+                if (occur == arrMot.Length)                         // si tout le mot est trouver on le signal
+                {
+                    Console.WriteLine("Vous avez trouve le mot");
+                    for (int i = 0; i < arrMotCache.Length; i++)    // affichage du mot trouvé
+                    {
+                        Console.Write(arrMotCache.GetValue(i));
+                    }
+                    Console.WriteLine("");
+                }
             }
         }
-        if (flag == false)
+        if (bonneLettre == false)                                          // si une mauvaise lettre a ete trouver j'ote une vie (deuxieme condition de sortie)
         {
             count--;
         }
-        if (count == 0)
+        if (count == 0)                                             // si le nombre de vie est reduit a 0 "Game Over"
         {
+            Console.WriteLine("Mauvaise lettre");
             Console.WriteLine("Game Over");
-            break;
+            Console.WriteLine("Le mot etait ");
+            for (int i = 0; i < arrMot.Length; i++)                 // affichage du mot a trouver
+            {
+
+                Console.Write(arrMot.GetValue(i));
+            }
+            Console.WriteLine("");
         }
-        if (flag == false && count != 0)
+        if (bonneLettre == false && count != 0)                            // si une mauvaise lettre est trouver et qu'on a encore au moins 1 vie, affichage du message "mauvaise lettre" et du nombre de vie restant
         {
             Console.WriteLine("Mauvaise lettre essayer a nouveau");
+            Console.WriteLine("Il vous reste " + count + " vie");
+            Console.WriteLine("");
         }
-    } while (count != 0);
-
+        index++;
+    } while (count != 0 && occur != arrMot.Length);                 // je boucle tant que j'ai des vie et que je n'ai pas trouver le mot caché
 }
 
-attempt(str, count);
-
-//using static System.Net.Mime.MediaTypeNames;
-
-//var mot = new pendu();
-//mot.mot = "pouet";
-
-//char[] test3 = mot.motPendu();
-
-//string[] motArr = Array.ConvertAll(test3, x => x.ToString());
-//string[] motMasque = new string[motArr.Length];
-
-//var motCache = new motCache();
-//motCache.motCachArr = motArr;
-
-//string[] test4 = motCache.hiddenWord();
+attempt(motArr, motMasque, lettreUsed);
 
 
-//for (int i = 0; i < motMasque.Length; i++)
-//{
-//    motMasque[i] = ".";
-//    Console.Write(motMasque.GetValue(i));
-//}
+static Char[] motPendu(string mot)                                  // fonction qui transforme un string en array string
+{
+    char[] nbLettre = new char[mot.Length];
+    for (int i = 0; i < mot.Length; i++)
+    {
+        nbLettre[i] = mot[i];
+    }
+    return nbLettre;
+}
+
+static string[] hiddenWord(string []motArr)                         // fonction qui me renvoie un tableau remplis de point de la meme longeur qu'un autre tableau
+{
+    string[] motMasque = new string[motArr.Length];
+    for (int i = 0; i < motMasque.Length; i++)
+    {
+        motMasque[i] = ".";
+    }
+    return motMasque;
+}

2710ad31ab6020569a7c38308b23f64a8901de5a
Lukraste <49816981+Lukraste@users.noreply.github.com>
2022-10-16 23:59:55 +0200

    V2 Finale

--- Lebègue Sébastien/Program.cs
+++ Lebègue Sébastien/Program.cs
@@ -1,80 +1,121 @@
 ﻿using System.Text;
 
-public class App
+namespace HangMan
 {
-    public static void Main()
+    public class Game
     {
-        //Mot choisit au hasard parmi une liste
-        string[] words = new string[] {"chouffe", "duvel", "triple karmeliet", "orval", "barbar"};
-        string response = "";
-        int score = 6;
-        bool win = false;
-        Random random = new Random();
-        var randomIndex = random.Next(0, words.Length);
-        string hiddenWord = words[randomIndex];
+        //Déclaration des variables publiques
+        public string[] words ;
+        public string response = "";
+        public int score;
+        public bool win;
+        public Random random;
+        public int randomIndex;
+        public string hiddenWord;
+        public bool toggle;
+        public string str;
+        public char userInput;
+        public StringBuilder temp;
 
-        //Creation de la response cachée par des underscores
-        for(var i=0 ; i<hiddenWord.Length ; i++)
+        //Création de la lise de mots du pendu
+        public void WordList()
         {
-            if(Char.IsWhiteSpace(hiddenWord[i]))
-            {
-                response += " ";
-            }
-            else 
-            {
-                response += "_";
-            }
+            this.words = new string[] {"chouffe", "duvel", "triple karmeliet", "orval", "barbar"};
         }
-
-        //Verification si la lettre est presente dans le mot
-        while(score != 0)
+        //Choix du mot au hasard
+        public void RandomWord()
         {
-            bool toggle = false;
-            Console.WriteLine("Tentatives restantes : " + score);
-            Console.WriteLine(response + "\n");
-            Console.WriteLine("Quelle lettre ?");
-            string str = Console.ReadLine();
-            char userInput = char.Parse(str);
-
-            for(var i=0 ; i<hiddenWord.Length ; i++)
-            {         
-                if(userInput == response[i])
+            this.random = new Random();
+            randomIndex = random.Next(0, this.words.Length);
+            this.hiddenWord = this.words[this.randomIndex];
+        }
+        //Création du visuel du mot caché
+        public void HideWord()
+        {
+            for(int i=0 ; i<this.hiddenWord.Length ; i++)
+            {
+                if(Char.IsWhiteSpace(this.hiddenWord[i]))
                 {
-                    continue;
-                }       
-                if(hiddenWord[i] == userInput)
+                    this.response += "   ";
+                }
+                else 
                 {
-                    toggle = true;
-                    StringBuilder temp = new StringBuilder(response);
-                    temp[i] = userInput;
-                    response = temp.ToString();
-                    break;
+                    this.response += "_ ";
                 }
             }
-            //Checks si le mot est trouvé
-            if(hiddenWord == response)
-            {
-                win = true;
-                break;
-            }
-            //Sinon résultat d'un essai
-            else if(toggle)
-            {
-                Console.WriteLine("Bien joué");
-            }
-            else 
-            {
-                Console.WriteLine("Raté !");
-                score--;
-            } 
-        } 
-        if(win)
-        {
-        Console.WriteLine("Vous avez trouvé le mot !\n" + response);
         }
-        else
+        //Verification si la lettre est presente dans le mot
+        public void CheckAnswer()
         {
-            Console.WriteLine("Game Over");
+            this.score = 6;
+            this.toggle = false;
+            this.win = false;
+
+            while(this.score != 0 || this.win == true)
+            {
+                this.toggle = false;
+                Console.WriteLine("Tentatives restantes : " + this.score);
+                Console.WriteLine(this.response + "\n");
+                Console.WriteLine("Quelle lettre ?");
+                this.str = Console.ReadLine();
+                this.userInput = char.Parse(this.str);
+
+                for(int i=0 ; i<this.hiddenWord.Length ; i++)
+                {         
+                    if(this.userInput == this.response[i])
+                    {
+                        continue;
+                    }       
+                    if(this.hiddenWord[i] == this.userInput)
+                    {
+                        this.toggle = true;
+                        this.temp = new StringBuilder(this.response);
+                        this.temp[i] = this.userInput;
+                        this.response = this.temp.ToString();
+                        break;
+                    }
+                }
+                //Affichage du résultat de la tentative
+                if(this.hiddenWord == this.response)
+                {
+                    this.win = true;
+                }
+                else if(this.toggle)
+                {
+                    Console.WriteLine("Bien joué jeune homme !");
+                }
+                else 
+                {
+                    Console.WriteLine("Raté ! Réessaye");
+                    this.score--;
+                } 
+            } 
+        }
+        //Vérifier le résultat et afficher le message finale
+        public void CheckResult()
+        {
+            if(this.win)
+            {
+                Console.WriteLine("Vous avez trouvé le mot !\n" + response);
+            }
+            else
+            {
+                Console.WriteLine("Game Over...");
+            }
+        }
+        public void play()
+        {
+            WordList();
+            RandomWord();
+            HideWord();
+            CheckAnswer();
+            CheckResult();
+        }
+        public static void Main()
+        {
+            Game game_01 = new Game();
+            game_01.play();          
         }
     }
 }
+

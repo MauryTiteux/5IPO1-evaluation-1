d43199b2f9edc761ece5ed799d423300e43bbe9c
Fabrice Kerremans <[200~kerremans.1904.student@ifosupwavre.[200~^[[200~kerremans.1904.student@ifosupwavre>
2022-10-16 21:42:40 +0200

    Début du code pendu

--- kerremans_fabrice/pendu/pendu/Program.cs
+++ kerremans_fabrice/pendu/pendu/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, titolakala");
-Console.WriteLine("Fabrice");
+﻿string listes = "labo";
+int tentatives = 7;
+string affichage = "";
+string lettres_trouvees = "";
+
+for (int i = 0; i < listes.Count(); i++)
+{
+    if (listes == listes)
+    {
+        affichage += " _ ";
+
+    }
+}
+
+
+
+Console.WriteLine("\n Mot à deviner: ", affichage);
+Console.WriteLine(">>> Bienvenue dans le pendu <<< ");
+Console.WriteLine(affichage);
+
+while(tentatives > 0)
+{
+    Console.WriteLine();
+    Console.Write("Taper une lettre: ");
+    string propostion = Console.ReadLine();
+
+
+}

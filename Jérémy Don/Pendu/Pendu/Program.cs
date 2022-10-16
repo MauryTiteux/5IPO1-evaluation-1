185da5b66c671d52444dc095348336dc1a30351f
WonderMaker87 <jeremy.don@gmx.com>
2022-10-16 11:28:22 +0200

    Final

--- Jérémy Don/Pendu/Pendu/Program.cs
+++ Jérémy Don/Pendu/Pendu/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pendu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Word> words = new List<Word>();
+            words.Add(new Word("ajouter"));
+            GameInstance game = new GameInstance();
+            game.Play();
+        }
+    }
+}
